456a97d7b17be4640cd2db089b193d272ec333e1
Githubkaew <107169494+Githubkaew@users.noreply.github.com>
2025-05-21 17:34:43 +0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.announcement-card').forEach(card => {
-    card.addEventListener('click', () => {
-      const posterId = card.dataset.posterId;
-      window.location.href = `page2.html?poster=${posterId}`;
-    });
-  });
